b1f1d34f09e39243ae90681c6c14d856e7baf94b
SuperTZS <104627523+SuperTZS@users.noreply.github.com>
2025-04-01 20:41:13 +0800

    Update sinchew.ts

--- server/sources/sinchew.ts
+++ server/sources/sinchew.ts
@@ -11,7 +11,46 @@ interface SinChewPost {
   excerpt: string
 }
 
-export default defineSource(async () => {
+const latest_posts = defineSource(async () => {
+  const allPosts: SinChewPost[] = []
+  
+  // 使用 for 循环获取 3 页数据
+  for (let page = 1; page <= 3; page++) {
+    const url = `https://www.sinchew.com.my/ajx-api/latest_posts/?page=${page}`
+    const response = await myFetch(url)
+    
+    // 解析 JSON 数据
+    let posts: SinChewPost[]
+    if (typeof response === 'string') {
+      posts = JSON.parse(response)
+    } else if (response && typeof response.json === 'function') {
+      posts = await response.json()
+    } else {
+      posts = response // 如果已经是解析好的数据
+    }
+    
+    allPosts.push(...posts)
+  }
+
+  // 转换数据格式
+  return allPosts.map((post) => ({
+    id: post.ID.toString(),
+    title: post.title,
+    url: post.permalink,
+    mobileUrl: post.permalink,
+    summary: post.excerpt.trim(),
+    time: post.time_display,
+    image: post.image && proxyPicture(post.image, "encodeBase64URL"),
+    extra: {
+      category: post.cat,
+      categoryLink: post.catlink,
+    }
+  }))
+})
+
+//hot
+
+const hot_posts = defineSource(async () => {
   const allPosts: SinChewPost[] = []
   
   // 使用 for 循环获取 3 页数据
@@ -46,4 +85,10 @@ export default defineSource(async () => {
       categoryLink: post.catlink,
     }
   }))
+})
+
+export default defineSource({
+  "sinchew": hot_posts,
+  "sinchew-hot": hot_posts,
+  "sinchew-realtime": latest_posts
 })
